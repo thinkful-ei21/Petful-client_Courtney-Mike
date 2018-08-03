20143f2b190416b8e9fd12ae59e1b27713fc99f8
Courtney Adams <cadams8238@gmail.com>
2018-08-03 10:55:46 -0700

    added component folder and Dashboard component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
+import Dashboard from './components/Dashboard';
 
 class App extends Component {
   render() {
     return (
       <div>
-        hello world
+        <Dashboard
+          catToAdopt={}
+          dogToAdopt={}
+        />
       </div>
+
     );
   }
 }
---
+++ src/components/Dashboard.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export default function Dashboard(props) {
+
+  return (
+    <div>
+      {props.catToAdopt}
+      {props.dogToAdopt}
+
+      <header>
+        <h1>{name}</h1>
+        <img src={} alt={}/>
+      </header>
+      <main>
+        <dl>
+          <
+        </dl>
+      </main>
+    </div>
+  );
+}
