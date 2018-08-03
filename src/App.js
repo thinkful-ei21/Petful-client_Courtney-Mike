7b2d316300a75904a5c5e8e6b92d604416264ec0
Courtney Adams <cadams8238@gmail.com>
2018-08-03 11:07:07 -0700

    dashboard component rendering dummy data correctly for both dog and cat

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
 import React, { Component } from 'react';
 import Dashboard from './components/Dashboard';
 
+const cat = {
+  imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg',
+  imageDescription: 'Orange bengal cat with black stripes lounging on concrete.',
+  name: 'Fluffy',
+  sex: 'Female',
+  age: 2,
+  breed: 'Bengal',
+  story: 'Thrown on the street'
+};
+
+const dog = {
+  imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+  imageDescription: 'A smiling golden-brown golden retreiver listening to music.',
+  name: 'Zeus',
+  sex: 'Male',
+  age: 3,
+  breed: 'Golden Retriever',
+  story: 'Owner Passed away'
+}
+
+
 class App extends Component {
   render() {
     return (
       <div>
         <Dashboard
-          catToAdopt={}
-          dogToAdopt={}
+          catToAdopt={cat}
+          dogToAdopt={dog}
         />
       </div>
 
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -4,18 +4,59 @@ export default function Dashboard(props) {
 
   return (
     <div>
-      {props.catToAdopt}
-      {props.dogToAdopt}
+      <section>
+        <header>
+          <h1>{props.catToAdopt.name}</h1>
+          <img
+            src={props.catToAdopt.imageURL}
+            alt={props.catToAdopt.imageDescription}
+          />
+        </header>
+        <main>
+          <p>More about {props.catToAdopt.name}</p>
+          <dl>
+            <dt>Sex</dt>
+            <dl>{props.catToAdopt.sex}</dl>
 
-      <header>
-        <h1>{name}</h1>
-        <img src={} alt={}/>
-      </header>
-      <main>
-        <dl>
-          <
-        </dl>
-      </main>
+            <dt>Age</dt>
+            <dl>{props.catToAdopt.age}</dl>
+
+            <dt>Breed</dt>
+            <dl>{props.catToAdopt.breed}</dl>
+
+            <dt>Story</dt>
+            <dl>{props.catToAdopt.story}</dl>
+          </dl>
+          <button>Adopt</button>
+        </main>
+      </section>
+
+      <section>
+        <header>
+          <h1>{props.dogToAdopt.name}</h1>
+          <img
+            src={props.dogToAdopt.imageURL}
+            alt={props.dogToAdopt.imageDescription}
+          />
+        </header>
+        <main>
+          <p>More about {props.dogToAdopt.name}</p>
+          <dl>
+            <dt>Sex</dt>
+            <dl>{props.dogToAdopt.sex}</dl>
+
+            <dt>Age</dt>
+            <dl>{props.dogToAdopt.age}</dl>
+
+            <dt>Breed</dt>
+            <dl>{props.dogToAdopt.breed}</dl>
+
+            <dt>Story</dt>
+            <dl>{props.dogToAdopt.story}</dl>
+          </dl>
+          <button>Adopt</button>
+        </main>
+      </section>
     </div>
   );
 }
