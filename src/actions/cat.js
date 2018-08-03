5d3bae6f88ef613a8ed9382ac787f27708c7ed41
Michael Le-Reiver <m.lereiver@gmail.com>
2018-08-03 11:56:32 -0700

    created actions for cat and dog

---
+++ src/actions/cat.js
@@ -0,0 +1,64 @@
+import { API_BASE_URL } from "../src/config";
+
+export const FETCH_CAT_REQUEST = 'FETCH_CAT_REQUEST';
+export const fetchCatRequest = () => ({
+  type: FETCH_CAT_REQUEST
+});
+
+export const FETCH_CAT_SUCCESS = 'FETCH_CAT_SUCCESS';
+export const fetchCatSuccess = cat => ({
+  type: FETCH_CAT_SUCCESS,
+  cat
+});
+
+export const FETCH_CAT_ERROR = 'FETCH_CAT_ERROR';
+export const fetchCatError = error => ({
+  type: FETCH_CAT_ERROR,
+  error: 'Cat could not be found'
+});
+
+
+export const DELETE_CAT_REQUEST = 'DELETE_CAT_REQUEST';
+export const deleteCatRequest = () => ({
+  type: DELETE_CAT_REQUEST
+});
+
+export const DELETE_CAT_SUCCESS = 'DELETE_CAT_SUCCESS';
+export const deleteCatSuccess = cat => ({
+  type: DELETE_CAT_SUCCESS,
+  cat
+});
+
+export const DELETE_CAT_ERROR = 'DELETE_CAT_ERROR';
+export const deleteCatError = error => ({
+  type: DELETE_CAT_ERROR,
+  error: 'Cat could not be found'
+});
+
+export const fetchCat = () => dispatch => {
+  dispatch(fetchCatRequest());
+  fetch('http://localhost:8080/api/cat/')
+    .then(res => res.json())
+    .then(data => dispatch(fetchCatSuccess(data)))
+    .catch(err => dispatch(fetchCatError(err)))
+}
+
+
+export const deleteCat = () => dispatch => {
+  dispatch(deleteCatRequest())
+  fetch(`${API_BASE_URL}/api/cat`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+  .then(res => {
+    if(!res.ok) {
+      return Promise.reject(res.statusText);
+    }
+    return res;
+  })
+  .then(res => res.sendStatus(204))
+  .then(data => dispatch(deleteCatSuccess(data)))
+  .catch(err => dispatch(deleteCatError(err)))
+}
---
+++ src/actions/dog.js
@@ -0,0 +1,62 @@
+import { API_BASE_URL } from "../src/config";
+
+export const FETCH_DOG_REQUEST = 'FETCH_DOG_REQUEST';
+export const fetchDogRequest = () => ({
+  type: FETCH_DOG_REQUEST
+});
+
+export const FETCH_DOG_SUCCESS = 'FETCH_DOG_SUCCESS';
+export const fetchDogSuccess = dog => ({
+  type: FETCH_DOG_SUCCESS,
+  dog
+});
+
+export const FETCH_DOG_ERROR = 'FETCH_DOG_ERROR';
+export const fetchDogError = error => ({
+  type: FETCH_DOG_ERROR,
+  error: 'Dog could not be found'
+});
+
+export const DELETE_DOG_REQUEST = 'DELETE_DOG_REQUEST';
+export const deleteDogRequest = () => ({
+  type: DELETE_DOG_REQUEST
+});
+
+export const DELETE_DOG_SUCCESS = 'DELETE_DOG_SUCCESS';
+export const deleteDogSuccess = dog => ({
+  type: DELETE_DOG_SUCCESS,
+  dog
+});
+
+export const DELETE_DOG_ERROR = 'DELETE_DOG_ERROR';
+export const deleteDogError = error => ({
+  type: DELETE_DOG_ERROR,
+  error: 'Dog could not be found'
+});
+
+export const fetchDog = () => dispatch => {
+  dispatch(fetchDogRequest());
+  fetch('http://localhost:8080/api/dog/')
+    .then(res => res.json())
+    .then(data => dispatch(fetchDogSuccess(data)))
+    .catch(err => dispatch(fetchDogError(err)))
+}
+
+export const deleteDog = () => dispatch => {
+  dispatch(deleteDogRequest())
+  fetch(`${API_BASE_URL}/api/dog`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+  .then(res => {
+    if(!res.ok) {
+      return Promise.reject(res.statusText);
+    }
+    return res;
+  })
+  .then(res => res.sendStatus(204))
+  .then(data => dispatch(deleteDogSuccess(data)))
+  .catch(err => dispatch(deleteDogError(err)))
+}
