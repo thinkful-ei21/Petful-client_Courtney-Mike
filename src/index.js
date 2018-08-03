0343cf8d2134f60b7ef7c11ee26962d1d0b23ff9
Courtney Adams <cadams8238@gmail.com>
2018-08-03 12:21:13 -0700

    added store to index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from 'react-redux';
+
 import App from './App';
 import Dashboard from '../src/components/Dashboard';
+import store from './store';
 import registerServiceWorker from './registerServiceWorker';
+import './index.css';
+
 
 ReactDOM.render(
-  <App >
-    <Dashboard />
-  </App>, document.getElementById('root'));
+  <Provider store={store}>
+    <App />
+  </Provider>, document.getElementById('root'));
 registerServiceWorker();
